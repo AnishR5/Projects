76a3a992a7578a386b78ca976a7ddca7f3909fd5
Anish Rachcha <anishrachcha@gmail.com>
2023-07-24 22:23:27 +0530

    ongoing

---
+++ portfolio/src/components/topbar/Topbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Topbar.scss'
+
+export default function Topbar() {
+  return (
+    <div className='topbar'>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className='logo'>Anish Rachcha</a>
+          <div className="itemContainer">
+            
+          </div>
+        </div>
+      
+        <div className="right">
+        
+        </div>
+        
+      </div>
+      
+    </div>
+  )
+}
